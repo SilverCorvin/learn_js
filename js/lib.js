9464dc8a1b2f9acfa43bd9782e32814429de0516
SilverCorvin <Ivan.Povalyaev@gmail.com>
2016-11-07 21:52:08 +0300

    lib.js was removed

--- js/lib.js
+++
@@ -1,8 +0,0 @@
-// Функция проверки значения на число
-function isNumeric(value) {
-    if (!isNaN(parseFloat(value)) && isFinite(value)) {
-        return true;
-    } else {
-        return false;
-    }
-}
