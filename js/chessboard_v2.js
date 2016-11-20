2afe245245bfca284df50848855c904b63ce563e
SilverCorvin <Ivan.Povalyaev@gmail.com>
2016-11-20 18:29:13 +0300

    version 0.8 was released

--- js/chessboard_v2.js
+++ js/chessboard_v2.js
@@ -47,14 +47,50 @@ function chessBoard() {
         }
     }
 
-    $('.code-data').append('<div class="step"></div>');
+    $('.code-data').append('<div class="step">Текущая ячейка:</div>');
+
+    $('.code-data').append('<div class="removed"></div>');
+
+    $('.removed').append('<table class="removed-white" cellspacing="0" cellpadding="0"></table>');
+    $('.removed-white').append('<tr></tr>');
+
+    for (let i = 0; i < 16; i++) {
+        $('.removed-white tr').append('<td></td>');
+    }
+
+    $('.removed').append('<table class="removed-black" cellspacing="0" cellpadding="0"></table>');
+    $('.removed-black').append('<tr></tr>');
+
+    for (let i = 0; i < 16; i++) {
+        $('.removed-black tr').append('<td></td>');
+    }
+
+    $('.chessBoard tr:eq(0) td:eq(0)').append('<img src="img/black_rook.png" alt="b_rook" height="48px" width="48px">');
+    $('.chessBoard tr:eq(0) td:eq(7)').append('<img src="img/black_rook.png" alt="b_rook" height="48px" width="48px">');
+    $('.chessBoard tr:eq(0) td:eq(1)').append('<img src="img/black_knight.png" alt="b_knight" height="48px" width="48px">');
+    $('.chessBoard tr:eq(0) td:eq(6)').append('<img src="img/black_knight.png" alt="b_knight" height="48px" width="48px">');
+    $('.chessBoard tr:eq(0) td:eq(2)').append('<img src="img/black_bishop.png" alt="b_bishop" height="48px" width="48px">');
+    $('.chessBoard tr:eq(0) td:eq(5)').append('<img src="img/black_bishop.png" alt="b_bishop" height="48px" width="48px">');
+    $('.chessBoard tr:eq(0) td:eq(3)').append('<img src="img/black_queen.png" alt="b_queen" height="48px" width="48px">');
+    $('.chessBoard tr:eq(0) td:eq(4)').append('<img src="img/black_king.png" alt="b_king" height="48px" width="48px">');
+    $('.chessBoard tr:eq(1) td').append('<img src="img/black_pawn.png" alt="b_pawn" height="48px" width="48px">');
+
+    $('.chessBoard tr:eq(7) td:eq(0)').append('<img src="img/white_rook.png" alt="w_rook" height="48px" width="48px">');
+    $('.chessBoard tr:eq(7) td:eq(7)').append('<img src="img/white_rook.png" alt="w_rook" height="48px" width="48px">');
+    $('.chessBoard tr:eq(7) td:eq(1)').append('<img src="img/white_knight.png" alt="w_knight" height="48px" width="48px">');
+    $('.chessBoard tr:eq(7) td:eq(6)').append('<img src="img/white_knight.png" alt="w_knight" height="48px" width="48px">');
+    $('.chessBoard tr:eq(7) td:eq(2)').append('<img src="img/white_bishop.png" alt="w_bishop" height="48px" width="48px">');
+    $('.chessBoard tr:eq(7) td:eq(5)').append('<img src="img/white_bishop.png" alt="w_bishop" height="48px" width="48px">');
+    $('.chessBoard tr:eq(7) td:eq(3)').append('<img src="img/white_queen.png" alt="w_queen" height="48px" width="48px">');
+    $('.chessBoard tr:eq(7) td:eq(4)').append('<img src="img/white_king.png" alt="w_king" height="48px" width="48px">');
+    $('.chessBoard tr:eq(6) td').append('<img src="img/white_pawn.png" alt="w_pawn" height="48px" width="48px">');
 
     function extractCell() {
         let letter = '',
             num = 0;
         letter = letters[$('.checked').index()];
         num = nums[$('.checked').parent().index()];
-        $('.step').text(letter + num);
+        $('.step').text(`Текущая ячейка: ${letter + num}`);
         return letter + num;
     }
 
@@ -71,8 +107,28 @@ function chessBoard() {
         }
     }
 
+    function removeImg(event, cell = undefined) {
+        let tr = 0,
+            td = 0;
+        if (cell !== undefined) {
+            tr = (8 - (+cell[1]) === 8 ? 0 : 8 - (+cell[1]));
+            td = (cell[0].charCodeAt(0) - 65 === 8 ? 0 : cell[0].charCodeAt(0) - 65);
+            $(`.chessBoard tr:eq(${tr}) td:eq(${td}) img`).css('display', 'none');
+        } else {
+            $(this).css('display', 'none');
+        }
+
+    }
+
+    function addImage(event, cell = undefined) {
+        let tr = 0,
+            td = 0;
+
+    }
+
     $('.chessBoard tr td').on('click', checkCell);
     $('.chessBoard tr td').on('click', extractCell);
+    $('.chessBoard tr td img').on('click', removeImg);
     $(document).keydown(function(e) {
 
         switch (e.which) {
@@ -104,10 +160,15 @@ function chessBoard() {
                 checkCell(e, cell);
                 extractCell();
                 break;
+            case 13: //return
+                e.preventDefault();
+                cell = extractCell();
+                removeImg(e, cell);
             default:
                 return;
         }
     });
+
 
 }
 
--- js/footer.js
+++ js/footer.js
@@ -1,3 +1,3 @@
 'use strict';
 let date = new Date();
-document.getElementById("footer").innerHTML = '<p>&copy; 2015-' + date.getFullYear() + ' by Ivan Povalyaev</p>';
+$('#footer').append(`<p>&copy; ${date.getFullYear()} by Ivan Povalyaev</p>`);
